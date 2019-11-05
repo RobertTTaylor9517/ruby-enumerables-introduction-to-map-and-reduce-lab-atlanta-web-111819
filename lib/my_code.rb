4b513d372cdd6141c12a81865b539f1f39f957c3
inherited-window-0818 <robt15@hotmail.com>
2019-11-05 01:26:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
-# My Code here....
+def map_to_negativize(array){
+  i = 0 
+}
+
+def map_to_no_change(array){
+  
+}
+
+def map_to_double(array){
+  
+}
+
+def map_to_square(array){
+  
+}
+
+def reduce_to_total(array, given){
+  
+}
+
+def reduce_to_all_true(array){
+  
+}
+
+def reduce_to_any_true(array){
+  
+}
+
+
